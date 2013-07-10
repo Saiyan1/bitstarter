abe4e9e624186be1e838ceb15a55865594750db0
Saiyan1 <insauralde.ap@gmail.com>
2013-07-10 02:23:22 +0000

    vars added

--- web.js
+++ web.js
@@ -3,6 +3,8 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
+  var buf = new Buffer();
+  var fs = require("fs");  
   response.send( buf.toString(fs.readFileSync(index.html)) );
 });
 
